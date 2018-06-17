1a18434827d6238b731136e64550def849ea4a17
diana <chizhdiana1@gmail.com>
2018-06-17 23:09:43 +0300

    changes_to_4

--- lections/lection_4_arrays/homework/array.js
+++ lections/lection_4_arrays/homework/array.js
@@ -1,124 +1,149 @@
+
+//Создать массив А размерностью n. Заполнить случайными числами любом диапазоне. Например A = [23,1,2,52,5,34,23,6,246,436];
+
+//проверить все числа на простоту, и найденные простые числа сохранить в массив B.
+
 var A = [];
-var B =[];
-A.length =10;
-var n =20;
+var B = [];
+A.length = 10;
+var n = 20;
 var m = 200;
 
+for (i = 0; i < A.length; i++) {
+    A[i] = Math.floor(Math.random() * (m - n)) + n;
 
-// Tast 1
+}
+console.log('A', A);
 
-// fill B using A
+for (j = 0; j < A.length; j++) {
+    //B = [];
+    if (A[j] % 2 != 0) {
 
-for(i=0;i< A.length; i++){
-    A[i]= Math.floor(Math.random()*(m - n)) + n;
+        res = A[j];
+
+        B.push(res);
+        // console.log(B)
 
     }
-    console.log('A',A);
-  
- for(j=0; j< A.length; j++){
-     //B = [];
-    if(A[j]% 2 !=0){
-       
-        res = A[j];
-        
-        B.push(res);
-       // console.log(B)
-    
-     } 
 
- }
+}
 
 console.log('B', B);
 
 
-//find max, min
-
+//найти максимальное число и минимальное число.
 
 var max = A[0];
-for(i= 0; i<A.length; i++){
- 
-if(A[i]>max){
-    max = A[i];
-   //
-    A[i]=max;   
+var min = A[0];
 
-}
+for (i = 0; i < A.length; i++) {
+
+    if (A[i] > max) {
+        max = A[i];
+
+        A[i] = max;
+
+    }
 }
 console.log('Max', max);
 
 
-var min = A[0];
-for(i= 0; i<A.length; i++){
-    
-if(A[i]<min){
-    min = A[i];
-    
+//
+for (i = 0; i < A.length; i++) {
 
+    if (A[i] < min) {
+        min = A[i];
+
+
+    }
 }
-}
-console.log('Min',min);
+console.log('Min', min);
 
 
 
 
 // Task 2
-//Beetwen_min_max
-for(i>min; i<max; i++){
+//Элементы массива между min -- max записать в массив B. Массив взять из 1го задания
 
-    B.push(i)
-    
+
+for (i = 0; i < A.length; i++) {
+
+
+    if (A[i] >= min && A[i] <= max) {
+
+        console.log('fgg' + min, max);
+
+        B.push(A[i]);
+
+    } else if (A[i] >= max && A[i] <= min) {
+
+        B.push(A[i]);
+
+    }
+
+    //
+
 
 }
-console.log(B);
+console.log('Apdated ', B);
 
 
 
 // Task 3
 
-// change max and min
+// В массиве A поменять местами min и max. Массив взять из 2го задания
 
 var b_min = B[0];
 var b_max = B[0];
-var nMin =0;
-var nMax =0;
 
-for(i=0; i<B.length; i++){
-    if(B[i]>max){
-        max = B[i];
-        nMax =i;
-        console.log('nMax', nMax);
+for (i = 0; i < B.length; i++) {
+    if (B[i] > b_max) {
+        b_max = B[i];
 
 
     }
-    if(B[i]<min){
-        min = B[i];
-        nMin =i;
-        console.log('nMin', nMin);
+    if (B[i] < b_min) {
+        b_min = B[i];
 
     }
-
-
 
 }
-var tmp = B[nMax];
-B[nMax] = B[nMin];
-B[nMin] = tmp;
 
-console.log('tmp', tmp);
+[b_min, b_max] = [b_max, b_min];
+
+
+console.log('b_min', b_min);
+console.log('b_max', b_max);
+
 
 
 
 // Task 4
-// revers A
+// Перевернуть массив, т.е. если был массив 1, 5, 6, 2, 4 -- то мы должны получить 4, 2, 6, 5, 1. Нельзя использовать стандартный метод reverse(). Постарайтесь не использовать дополнительный массив. Оригинальный массив А сохранять не нужно (т.е. он должен перевернуться).
 
-for(i= A.length-1 ; i>0; i--){
+for (i = A.length - 1; i > 0; i--) {
     console.log('Reversed A', A[i]);
+
 }
 
 
+//Определить количество элементов в заданном массиве, отличающихся от минимального на 5.
+count = 0;
+min = 10;
+arr = [1, 2, 3, 4, 5, 10, 5, 15, 15, 10]
 
 
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] == min + 5 || arr[i] == min - 5) {
 
+
+        count++;
+
+    }
+
+}
+
+console.log('count', count);
 
 
 
