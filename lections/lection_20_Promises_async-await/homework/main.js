692777ab8e162348f3e50a9b3c8c54a77bc20d94
diana <chizhdiana1@gmail.com>
2018-10-06 10:44:14 +0300

    lections_20

--- lections/lection_20_Promises_async-await/homework/main.js
+++ lections/lection_20_Promises_async-await/homework/main.js
@@ -1,21 +1,21 @@
 
 
 
-function doAjax(method, url){
+function doAjax(method, url) {
     var xhr = new XMLHttpRequest();
 
     xhr.open(method, url, true);
 
-    return new Promise(function(resolve, reject){
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState != 4) {
+    return new Promise(function (resolve, reject) {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) {
                 return;
             }
 
             var resp = JSON.parse(xhr.responseText);
 
             if (xhr.status != 200) {
-                
+
                 reject(xhr.statusText);
             }
 
@@ -32,89 +32,94 @@ var listPomise = [doAjax('GET', '/data.json'), doAjax('GET', '/data2.json')];
 
 
 Promise.all(listPomise)
-.then(function(values){
-   // console.log(values);
-    var res =[]
-    for(i in values){
-        res= JSON.stringify( values[i].concat(values[i]))
-    }
-   
-    console.log('res'+ res)
-    return res;
-    
-})
-.catch(function(err){
-    console.log(err);
-});
+    .then(function (values) {
+        // console.log(values);
+        var res = []
+        for (i in values) {
+            res = JSON.stringify(values[i].concat(values[i]))
+        }
+
+        console.log('res' + res)
+        return res;
+
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 //-------------------------
 
 
-function runAfter(){
-    var fun =[];
-    for( i =0 ; i <arguments.length; i++){
-        for(j=0; j< arguments[i].length; j++){
-          
-            fun.push( arguments[i][j]) 
-         }   
-        }
-         
-         return function(values){
-             for(var j= 0; j<fun.length;j++){
-                values.forEach(element => {
-                   fun[j](element)
-                    
-                });
+function runAfter() {
+    // console.log(arguments, arguments)
 
-             }
-         
-         
-        
+    var funk;
+    for (i in arguments) {
 
-     
-}
-   
-}
-        
+        funk = arguments[i]
+    }
 
-function doIt(values){
-    console.log('doIt',values);
-    return this.values;
-
-}
-function doIt1(values){
-    console.log('doIt1',values);
-    return this.values+1;
-
-}
-function doIt2(values){
-    console.log('doIt2',values);
-    return this.values+2;
-
-}
-
-function gPr(callback){
+    for (j = 0; j < funk.length; j++) {
+        var f = funk[j]
     
+        return function (values) {
+           // var values = arguments
+          //  return f(values)
+        for (k = 0; k < values.length; k++) {
+              
+                var result = f(values[k])
+                console.log('res',result)
 
-    return new Promise(function(resolve, reject){
+
+            }
+        }
+
+    }
+
+    }
+
+
+
+
+function doIt(values) {
+    console.log('doIt', values);
+
+    this.values = values + 1;
+    return this
+
+}
+function doIt1(values) {
+    console.log('doIt1', values);
+    this.values = values + 2;
+    return this
+}
+function doIt2(values) {
+    console.log('doIt2', values);
+    this.values = values + 3;
+    return this
+
+}
+
+function gPr(callback) {
+    return new Promise(function (resolve, reject) {
         callback(resolve, reject);
-         
+
     })
 }
-function setTimer(resolve, reject){
-    setTimeout(function(){
-       
-        var local = Math.floor(Math.random()*100) + 1
+function setTimer(resolve, reject) {
+    setTimeout(function () {
+
+        var local = Math.floor(Math.random() * 100) + 1
         if (local <= 100) {
             console.log('local', local)
             resolve(local);
-         }
+        }
 
         reject(local);
 
     }, 500);
 }
-var listgPR = [gPr(setTimer),gPr(setTimer), gPr(setTimer), gPr(setTimer)]
+var listgPR = [gPr(setTimer), gPr(setTimer), gPr(setTimer), gPr(setTimer)]
 
 Promise.all(listgPR)
-.then(runAfter([doIt, doIt1, doIt2]));
+    .then(runAfter([doIt, doIt1, doIt2]));
 
